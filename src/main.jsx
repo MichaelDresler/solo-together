e82c72963af533f9b910f9237fd93aea9fff7421
Michael Dresler <michaeldresler@hotmail.com>
2026-03-02 01:48:34 -0800

    feature: added pages folder for routes & setup more testing routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
 import "./index.css";
-import App from "./App.jsx";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
+import RootLayout from "./layout/RootLayout.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "login", element: <Login /> },
+    ],
   },
+  {},
 ]);
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-  <RouterProvider
-  router={router}
-  />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
