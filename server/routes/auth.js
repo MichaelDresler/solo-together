87f31ef66a1404488f361ef52da0e755e15dc92d
Michael Dresler <michaeldresler@hotmail.com>
2026-03-04 15:31:31 -0800

    feature: added login route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,7 @@
 import express from "express";
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
-
+import jwt from "jsonwebtoken";
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
@@ -27,9 +27,40 @@ router.post("/register", async (req, res) => {
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = await User.create({ username, hashedPassword });
-    console.log(`new user created: ${newUser}`)
+    console.log(`new user created: ${newUser}`);
 
-    return res.status(201).json({ user: { id: newUser._id, username: newUser.username } });
+    return res
+      .status(201)
+      .json({ user: { id: newUser._id, username: newUser.username } });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ error: "server error" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    // validation
+    if (!username || !password) {
+      return res
+        .status(400)
+        .json({ error: "please provide a username or password" });
+    }
+    const user = await User.findOne({ username });
+    // validation
+    if (!user) return res.status(404).json({ error: "no such user exists" });
+
+    // validation
+    const passwordMatches = await bcrypt.compare(password, user.hashedPassword);
+    if (!passwordMatches)
+      return res.status(401).json({ error: "invalid credentials" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_TOKEN, {
+      expiresIn: "1h",
+    });
+
+    res.json({ token, user: { id: user._id, username: user.username } });
 
   } catch (e) {
     console.log(e);
